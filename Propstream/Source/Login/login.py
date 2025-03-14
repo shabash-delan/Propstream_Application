1b9a3fba44abee4f3eefb309a58c49d0b2786f8d
shabash-delan <shabashs@delan.tech>
2025-03-14 16:08:04 +0530

    First commit

--- Propstream/Source/Login/login.py
+++ Propstream/Source/Login/login.py
@@ -1,1 +1,31 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import configparser
+
+
+
+config=configparser.RawConfigParser()
+config.read("Propstream/Utils/config.properties")
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.web = WebDriverWait(self.driver, 10)
+
+    def login(self):
+
+        username_field = self.web.until(EC.visibility_of_element_located((By.XPATH,'//input[@name="username"]')))
+        username_field.send_keys(config.get('ME', 'username'))
+        password_field = self.web.until(EC.visibility_of_element_located((By.XPATH,'//input[@name="password"]')))
+        password_field.send_keys(config.get('ME', 'password'))
+        time.sleep(1)
+        login_button = self.web.until(EC.element_to_be_clickable((By.XPATH, config.get('ME', 'Login_bt'))))
+        login_button.click()
+
+
+
 
---
+++ main.py
@@ -0,0 +1,34 @@
+from configparser import ConfigParser
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from Propstream.Source.Login.login import LoginPage
+
+
+
+def main():
+    config = ConfigParser()
+    config.read("Propstream/Utils/config.properties")
+
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+
+    driver.maximize_window()
+    url = config.get('ME', 'base_url')
+    if url:
+
+    # Login Page
+
+        driver.get(url)
+        login_page = LoginPage(driver)
+        login_page.login()
+
+        driver.quit()
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    main()
